07e97bfb323f4d43f38bf7203557ea92677d5016
Alexander Kozhenkin <chenzeyroyal@gmail.com>
2025-04-26 13:24:46 +0300

    My first commit

---
+++ script.js
@@ -0,0 +1,225 @@
+// --- THEME MODE ---
+
+const themeToggle = document.querySelector("[data-js-theme-toggle]");
+const themeSwitch = document.querySelector("[data-js-theme-switch]");
+
+themeToggle.addEventListener("click", () => {
+  const currentTheme = document.documentElement.getAttribute("data-js-theme");
+  newTheme = currentTheme === "light" ? "dark" : "light";
+  document.documentElement.setAttribute("data-js-theme", newTheme);
+  themeSwitch.classList.toggle("switch");
+});
+
+// --- BURGER BUTTON & MENU ---
+
+const burgerButton = document.querySelector("[data-js-burger]");
+const menu = document.querySelector("[data-js-menu]");
+
+burgerButton.addEventListener("click", () => {
+  burgerButton.classList.toggle("is-active");
+  menu.classList.toggle("is-active");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    menu.classList.remove("is-active");
+    burgerButton.classList.remove("is-active");
+  }
+});
+
+// --- HERO ANIMATION ---
+
+const paragraph = document.querySelector("[data-js-paragraph]");
+const text = "Привет. Я - Александр.";
+const textSecond = "\nFrontend-разработчик.";
+const textContainer = document.querySelector("[data-js-typewriter]");
+const description = document.querySelector("[data-js-description]");
+const contactButton = document.querySelector("[data-js-contact-button]");
+const nextButton = document.querySelector("[data-js-next-button]");
+
+const speed = 100;
+
+const textContainerSecond = document.createElement("span");
+
+// Функция delay создает промис, который разрешается через указанное количество времени.
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Функция становится ассинхронной - то есть, внутри нее можно использовать await
+
+async function typeWriter() {
+  const sequences = [
+    { text: text, container: textContainer },
+    {
+      text: textSecond,
+      container: textContainerSecond,
+      appendTo: textContainer,
+    },
+  ];
+
+  for (const { text, container, appendTo } of sequences) {
+    if (appendTo) {
+      appendTo.appendChild(container);
+      container.classList.add("hero__welcome-typewriter-second");
+    }
+
+    for (const char of text) {
+      container.innerHTML += char === "\n" ? "<br>" : char;
+      // Говорим функци typeWriter подождать момента, когда разрешится промис (пройдет заданное время). Т.к. это цикл, данная проверка на разрешение срабатывает до момента, пока не закончатся символы в тексте.
+
+      await delay(speed);
+    }
+    // Говорим функци typeWriter подождать момента, когда разрешится промис (пройдет заданное время). Т.к. это цикл, данная проверка на разрешение срабатывает до момента, пока не закончатся символы в тексте.
+  }
+  // Говорим функции typeWriter подождать ммоента, когда разрешится промис (пройдет заданное время). После того, как цикл завершается, функция останавливается и продолжается снова через заданное количество времени.
+  await delay(200);
+  description.classList.add("show");
+  await delay(1000);
+  nextButton.classList.add("show");
+  contactButton.classList.add("show");
+  await delay(1000);
+  nextButton.style.transition = "0.2s";
+  contactButton.style.transition = "0.2s";
+  paragraph.classList.add("show");
+}
+
+window.onload = typeWriter;
+
+// --- ROBOT ---
+
+const robot = {
+  wrapper: document.querySelector("[data-js-robot]"),
+  head: document.querySelector("[data-js-robotHead]"),
+  button: document.querySelector("[data-js-robotButton]"),
+  eyes: document.querySelectorAll("[data-js-robotEye]"),
+  arm: document.querySelector("[data-js-robotArm]"),
+  shadow: document.querySelector("[data-js-robotShadow]"),
+};
+
+robot.button.addEventListener("click", () => {
+  let interval = setInterval(() => {
+    const translateY =
+      parseFloat(getComputedStyle(robot.wrapper).transform.split(", ")[5]) || 0;
+    if (Math.round(translateY) === 0) {
+      clearInterval(interval);
+      robot.wrapper.classList.toggle("landed");
+      robot.shadow.classList.toggle("landed-shadow");
+    }
+  }, 100);
+  robot.eyes.forEach((eye) => {
+    eye.classList.toggle("landed");
+    eye.classList.remove("hide");
+    eye.classList.toggle("robot-sleep");
+  });
+  robot.head.classList.toggle("rotate-head");
+  robot.arm.classList.toggle("arm-wave");
+});
+
+robot.arm.addEventListener("animationstart", () => {
+  robot.eyes.forEach((eye) => {
+    eye.classList.add("hide");
+  });
+});
+robot.arm.addEventListener("animationend", () => {
+  robot.eyes.forEach((eye) => {
+    eye.classList.remove("hide");
+  });
+});
+
+robot.head.addEventListener("mouseover", () => {
+  robot.eyes.forEach((eye) => {
+    if (!eye.classList.contains("robot-sleep")) {
+      eye.classList.add("landed");
+      eye.classList.add("hide");
+    }
+  });
+});
+robot.head.addEventListener("mouseout", () => {
+  robot.eyes.forEach((eye) => {
+    if (!eye.classList.contains("robot-sleep")) {
+      eye.classList.remove("landed");
+      eye.classList.remove("hide");
+    }
+  });
+});
+
+const modal = document.querySelectorAll(".portfolio__modal");
+const thumbnail = document.querySelectorAll(".portfolio__image");
+const closeModalButton = document.querySelectorAll(".portfolio__modal-close");
+
+thumbnail.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    modal[index].classList.add("show-modal");
+    document.body.classList.add("modal-opened");
+
+    closeModalButton.forEach((button) => {
+      button.addEventListener("click", () => {
+        modal[index].classList.remove("show-modal");
+        document.body.classList.remove("modal-opened");
+      });
+    });
+  });
+});
+
+// PORTFOLIO SLIDER
+
+// const sliderLine = document.querySelector(".portfolio__list");
+// let slides = Array.from(document.querySelectorAll(".portfolio__list-item"));
+// const btnPrev = document.querySelector(".prev");
+// const btnNext = document.querySelector(".next");
+
+// const visibleCount = 3;
+// let index = visibleCount;
+
+// function cloneSlides() {
+//   const firstClones = slides
+//     .slice(0, visibleCount)
+//     .map((slide) => slide.cloneNode(true));
+//   const lastClones = slides
+//     .slice(-visibleCount)
+//     .map((slide) => slide.cloneNode(true));
+
+//   firstClones.forEach((clone) => sliderLine.appendChild(clone));
+//   lastClones
+//     .reverse()
+//     .forEach((clone) => sliderLine.insertBefore(clone, sliderLine.firstChild));
+//   console.log(lastClones);
+//   slides = Array.from(document.querySelectorAll(".portfolio__list-item"));
+// }
+
+// cloneSlides();
+
+// let slideWidth;
+// function updateSize() {
+//   slideWidth = slides[0].getBoundingClientRect().width + 30;
+//   sliderLine.style.transform = `translateX(-${index * slideWidth}px)`;
+// }
+// window.addEventListener("resize", updateSize);
+// updateSize();
+
+// function moveSlider() {
+//   sliderLine.style.transition = "transform 0.3s ease";
+//   sliderLine.style.transform = `translateX(-${index * slideWidth}px)`;
+// }
+
+// btnNext.addEventListener("click", () => {
+//   index++;
+//   moveSlider();
+// });
+
+// btnPrev.addEventListener("click", () => {
+//   index--;
+//   moveSlider();
+// });
+
+// sliderLine.addEventListener("transitionend", () => {
+//   if (index >= slides.length - visibleCount) {
+//     sliderLine.style.transition = "none";
+//     index = visibleCount;
+//     sliderLine.style.transform = `translateX(-${index * slideWidth}px)`;
+//   }
+//   if (index < visibleCount) {
+//     sliderLine.style.transition = "none";
+//     index = slides.length - visibleCount * 2;
+//     sliderLine.style.transform = `translateX(-${index * slideWidth}px)`;
+//   }
+// });
