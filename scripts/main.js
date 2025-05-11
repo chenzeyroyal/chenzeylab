d9aa1136a8b1c855a54a23c776b80df50c1a61ae
Alexander Kozhenkin <chenzeyroyal@gmail.com>
2025-05-11 20:26:09 +0300

    Modal fixes, link style

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-import "./burger.js";
-import "./modals.js";
-import "./observer.js";
-import "./hero.js";
-import "./theme.js";
-import "./robot.js";
--- scripts/theme.js
+++
@@ -1,9 +0,0 @@
-const themeToggle = document.querySelector("[data-js-theme-toggle]");
-const themeSwitch = document.querySelector("[data-js-theme-switch]");
-
-themeToggle.addEventListener("click", () => {
-  const currentTheme = document.documentElement.getAttribute("data-js-theme");
-  const newTheme = currentTheme === "light" ? "dark" : "light";
-  document.documentElement.setAttribute("data-js-theme", newTheme);
-  themeSwitch.classList.toggle("switch");
-});
